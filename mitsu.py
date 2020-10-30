48f5ed6a82b1b7fa31bf83e0563a0fe0748d5bf9
nono-n1I <nono6217n@gmail.com>
2020-10-30 22:06:36 +0900

    fix argument value

--- mitsu.py
+++ mitsu.py
@@ -18,53 +18,52 @@ while True:
     ret1, frame1 = cap1.read()
     ret2, frame2 = cap2.read()
 
-    frame1 = frame1[26:, 16:]
-    frame2 = frame2[:337, :626]
-    frame1 = cv2.copyMakeBorder(frame1, 13, 13, 8, 8, cv2.BORDER_CONSTANT, 0)
-    frame2 = cv2.copyMakeBorder(frame2, 12, 11, 7, 7, cv2.BORDER_CONSTANT, 0)
+    frame1 = frame1[11:, 15:]
+    # frame2 = frame2[:339, :]
+    frame1 = cv2.copyMakeBorder(frame1, 11, 0, 15, 0, cv2.BORDER_CONSTANT, 0)
+    # frame2 = cv2.copyMakeBorder(frame2, 0, 21, 0, 0, cv2.BORDER_CONSTANT, 0)
+    # mat = cv2.getRotationMatrix2D((320, 180), -2, 1)
+    # frame1 = cv2.warpAffine(frame1, mat, (640, 360))
 
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
     hogParams = {'winStride': (8, 8), 'padding': (32, 32), 'scale': 1.05}
     human1, r1 = hog.detectMultiScale(frame1, **hogParams)
     human2, r2 = hog.detectMultiScale(frame2, **hogParams)
-    # print(human1)
+
     point = []
     for(x, y, w, h) in human1:
         cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 0, 0), 3)
-        # cv2.circle(frame1, (x+w//2, y+h//2), 15, (0, 50, 255), thickness=-1)
         point.append((x+w//2, y+h//2))
 
     for(x, y, w, h) in human2:
         cv2.rectangle(frame2, (x, y), (x+w, y+h), (0, 50, 255), 3)
 
-    # cv2.imshow('frame1', frame1)
-    # cv2.imshow('frame2', frame2)
-
-    stereo = cv2.StereoBM_create(numDisparities=16, blockSize=23)
+    stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
     frame11 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     frame22 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
-    frame11 = cv2.GaussianBlur(cv2.equalizeHist(frame11),(7,7), 6)
-    frame22 = cv2.GaussianBlur(cv2.equalizeHist(frame22),(7,7), 6)
-    # out = stereo.compute(frame1, frame2).astype(np.float32) / 16
+    frame11 = cv2.GaussianBlur(cv2.equalizeHist(frame11),(15,15), 7)
+    frame22 = cv2.GaussianBlur(cv2.equalizeHist(frame22),(15,15), 7)
     out = stereo.compute(frame11, frame22)
 
 
     # cv2.imshow('frame1', frame1)
     # cv2.imshow('frame2', frame2)
+    # print(out)
+    # print(np.count_nonzero(out == -16))
+    # print('')
     out = np.where(out<0, 0, out)
     out = out.astype(np.uint8)
 
     sorted(point)
     for i in range(len(point) - 1):
-        # diff = point[i+1] - point[i]
         x0 = point[i][0]
         y0 = point[i][1]
         x1 = point[i+1][0]
         y1 = point[i+1][1]
 
-        if (abs(out[y1][x1] - out[y0][x0]) <= 60) and (abs(x1 - x0) <= 100):
+        if (abs(out[y1][x1] - out[y0][x0]) <= 60) and (abs(x1 - x0) <= 100) and (out[y1][x1] + out[y0][x0] != 0):
             print(cnt, "密です")
             subprocess.call("mpg321 mitsudesu.mp3", shell=True)
             cv2.circle(frame1, (x0, y0), 15, (0, 50, 255), thickness=-1)
@@ -80,6 +79,6 @@ while True:
     if cv2.waitKey(delay) & 0xFF == ord('q'):
         break
     time.sleep(1)
-cv2.imwrite('./frame1.png', frame1)
-cv2.imwrite('./frame2.png', frame2)
-cv2.imwrite('./out.png', out)
+# cv2.imwrite('./frame1.png', frame1)
+# cv2.imwrite('./frame2.png', frame2)
+# cv2.imwrite('./out.png', out)
